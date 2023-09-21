0b3c6ec7dceddf0868436d8b7d021b3a63fd90d4
EzatRashid <ezat.b.rashid@gmail.com>
2023-09-21 08:08:00 -0600

    did some javascript

--- index.js
+++ index.js
@@ -2,42 +2,57 @@ let cells = document.querySelectorAll('.cell')
 let statusText = document.querySelector('#statusText')
 let restartBtn = document.querySelector('#restart')
 
-const winConditions = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6],
+
+
+
+cells.forEach(cell =>{
+    let node = document.createTextNode();
+
+})
+cells.addEventListener("click",(cell) =>{
+    cell.innerHTML = 'X'
+})
+// const winConditions = [
+//     [0,1,2],
+//     [3,4,5],
+//     [6,7,8],
+//     [0,3,6],
+//     [1,4,7],
+//     [2,5,8],
+//     [0,4,8],
+//     [2,4,6],
     
-]
+// ]
 
-let options = ['','','','','','','','','','','']
+// let options = ['','','','','','','','','','','']
 
-let currentPlayer = 'X'
-let running = false
+// let currentPlayer = 'X'
+// let running = false
 
-const initializeGame= () =>{
-    cells.forEach(cell => cell.addEventListener('click',cellClicked ))
-    restartBtn.addEventListener('click',restartGame)
-    statusText.innerHTML = `${currentPlayer}'s turn`
-    running = true
-}
+// initializeGame()
+// cellClicked();
+// updateCell()
 
-function cellClicked(){
-    const cellIndex = this.getAttribute('cellIndex')
+// const initializeGame= () =>{
+//     cells.forEach(cell => cell.addEventListener('click',cellClicked ))
+//     restartBtn.addEventListener('click',restartGame)
+//     statusText.innerHTML = `${currentPlayer}'s turn`
+//     running = true
+// }
 
-    if(options[cellIndex !== '' || !running]){
-        return
-    }else{
-        updateCell(this,cellIndex);
-        checkWinner();
-    }
-}
 
-const updateCell = (cell,index) =>{
-    options[index] = currentPlayer;
-    cell.textContent = currentPlayer
-}
+// function cellClicked(){
+//     const cellIndex = this.getAttribute('cellIndex')
+
+//     if(options[cellIndex !== '' || !running]){
+//         return
+//     }else{
+//         updateCell(this,cellIndex);
+//         checkWinner();
+//     }
+// }
+
+// const updateCell = (cell,index) =>{
+//     options[index] = currentPlayer;
+//     cell.textContent = currentPlayer
+// }
