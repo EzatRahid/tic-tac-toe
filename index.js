a6de6774dfa375d63706b9fa19b90a04eb2834d2
EzatRashid <ezat.b.rashid@gmail.com>
2023-09-21 19:58:59 -0600

    finished the javascript

--- index.js
+++ index.js
@@ -5,34 +5,28 @@ let restartBtn = document.querySelector('#restart')
 
 let currentPlayer = 'X'
 
-cells.forEach(cell =>{
-    cell.addEventListener('click',()=>{
-        if(cell.innerHTML === ''){
-            cell.innerHTML = currentPlayer
-           if(currentPlayer === 'X'){
-            currentPlayer ='O'
-           }else if(currentPlayer === 'O'){
-            currentPlayer = 'X'
-           }
-           checkWin();
-        }
-    })
-})
-
-
-const winConditions = [
+const winConditions =[
     [0,1,2],
     [3,4,5],
     [6,7,8],
+    [0,4,8],
+    [2,4,6],
     [0,3,6],
     [1,4,7],
     [2,5,8],
-    [0,4,8],
-    [2,4,6],
-    
 ]
 
-
+cells.forEach(cell =>{
+    cell.addEventListener('click',()=>{
+        cell.innerHTML = currentPlayer;
+        if(currentPlayer ==='X'){
+            currentPlayer = 'O'
+        }else{
+            currentPlayer = 'X'
+        }
+        checkWin();
+    })
+})
 
 const checkWin = () =>{
     for(const condition of winConditions){
@@ -42,54 +36,21 @@ const checkWin = () =>{
         const cellB = cells[b].innerHTML
         const cellC = cells[c].innerHTML
 
-        if(cellA && cellA === cellB && cellA === cellC ){
-            statusText.innerHTML = `Player ${cellA}`
-            return;
+        if(cellA && cellA === cellB && cellA === cellC){
+            statusText.innerHTML = `Player ${cellA} has won!`
         }
-
     }
+    return;
 }
 
-let cellsFilled = true
+let allCellsFilled = true
 
 cells.forEach(cell =>{
     if(cell.innerHTML === ''){
-        cellsFilled = false
+        allCellsFilled =false
     }
 })
 
-if(cellsFilled === false){
-    statusText.innerHTML = `Game is draw!`
+if(!allCellsFilled){
+    statusText.innerHTML = `GAME IS A DRAW!!! `
 }
-// let options = ['','','','','','','','','','','']
-
-// let currentPlayer = 'X'
-// let running = false
-
-// initializeGame()
-// cellClicked();
-// updateCell()
-
-// const initializeGame= () =>{
-//     cells.forEach(cell => cell.addEventListener('click',cellClicked ))
-//     restartBtn.addEventListener('click',restartGame)
-//     statusText.innerHTML = `${currentPlayer}'s turn`
-//     running = true
-// }
-
-
-// function cellClicked(){
-//     const cellIndex = this.getAttribute('cellIndex')
-
-//     if(options[cellIndex !== '' || !running]){
-//         return
-//     }else{
-//         updateCell(this,cellIndex);
-//         checkWinner();
-//     }
-// }
-
-// const updateCell = (cell,index) =>{
-//     options[index] = currentPlayer;
-//     cell.textContent = currentPlayer
-// }
