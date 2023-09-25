f54e7af49a6a6941f6a81c63df9e0505a746b6bd
EzatRashid <ezat.b.rashid@gmail.com>
2023-09-25 17:30:06 -0600

    just practicing

--- index.js
+++ index.js
@@ -3,59 +3,97 @@ let statusText = document.querySelector('#statusText')
 let restartBtn = document.querySelector('#restart')
 
 
-let currentPlayer = 'X'
-
-const winConditions =[
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,4,8],
-    [2,4,6],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-]
-
-cells.forEach(cell =>{
-    cell.addEventListener('click',()=>{
-        cell.innerHTML = currentPlayer;
-        if(currentPlayer ==='X'){
-            currentPlayer = 'O'
-        }else{
-            currentPlayer = 'X'
-        }
-        checkWin();
-    })
-})
-
-restartBtn.addEventListener('click', () =>{
-    cells.forEach(cell =>{
-        cell.innerHTML = ''
-    })
-    statusText.innerHTML = ''
-})
 
 
-const checkWin = () =>{
-    for(const condition of winConditions){
-        const [a,b,c] = condition
 
-        const cellA = cells[a].innerHTML
-        const cellB = cells[b].innerHTML
-        const cellC = cells[c].innerHTML
 
-        if(cellA && cellA === cellB && cellA === cellC){
-            statusText.innerHTML = `Player ${cellA} has won!`
-        }
-    }
-    return;
-}
 
-let allCellsFilled = true
 
-cells.forEach(cell =>{
-    if(cell.innerHTML === ''){
-        allCellsFilled =false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let currentPlayer = 'X'
+
+// const winConditions =[
+//     [0,1,2],
+//     [3,4,5],
+//     [6,7,8],
+//     [0,4,8],
+//     [2,4,6],
+//     [0,3,6],
+//     [1,4,7],
+//     [2,5,8],
+// ]
+
+// cells.forEach(cell =>{
+//     cell.addEventListener('click',()=>{
+//         cell.innerHTML = currentPlayer;
+//         if(currentPlayer ==='X'){
+//             currentPlayer = 'O'
+//         }else{
+//             currentPlayer = 'X'
+//         }
+//         checkWin();
+//     })
+// })
+
+// restartBtn.addEventListener('click', () =>{
+//     cells.forEach(cell =>{
+//         cell.innerHTML = ''
+//     })
+//     statusText.innerHTML = ''
+//     currentPlayer = 'X'
+// })
+
+
+// const checkWin = () =>{
+//     for(const condition of winConditions){
+//         const [a,b,c] = condition
+
+//         const cellA = cells[a].innerHTML
+//         const cellB = cells[b].innerHTML
+//         const cellC = cells[c].innerHTML
+
+//         if(cellA && cellA === cellB && cellA === cellC){
+//             statusText.innerHTML = `Player ${cellA} has won!`
+//         }
+//     }
+//     return;
+// }
+
+// let allCellsFilled = true
+
+// cells.forEach(cell =>{
+//     if(cell.innerHTML === ''){
+//         allCellsFilled =false
     }
 })
 
